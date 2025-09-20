5a9657bcd45aee43f0298fc24dc14d286189bd1b
glenda-kv <karolvediag@gmail.com>
2025-09-20 14:50:00 -0400

    Se añadio los formularios con su estructura basica de Repuesto,Cliente y Equipo.

--- ProyectoFinal_P3/FrmInicio.Designer.cs
+++ ProyectoFinal_P3/FrmInicio.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            administradorToolStripMenuItem = new ToolStripMenuItem();
+            clienteToolStripMenuItem = new ToolStripMenuItem();
+            equpoToolStripMenuItem = new ToolStripMenuItem();
+            inventarioToolStripMenuItem = new ToolStripMenuItem();
+            repuestoToolStripMenuItem = new ToolStripMenuItem();
+            inventarioToolStripMenuItem1 = new ToolStripMenuItem();
+            facturaToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { administradorToolStripMenuItem, clienteToolStripMenuItem, equpoToolStripMenuItem, inventarioToolStripMenuItem, repuestoToolStripMenuItem, inventarioToolStripMenuItem1, facturaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(897, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // administradorToolStripMenuItem
+            // 
+            administradorToolStripMenuItem.Name = "administradorToolStripMenuItem";
+            administradorToolStripMenuItem.Size = new Size(118, 24);
+            administradorToolStripMenuItem.Text = "Administrador";
+            administradorToolStripMenuItem.Visible = false;
+            // 
+            // clienteToolStripMenuItem
+            // 
+            clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            clienteToolStripMenuItem.Size = new Size(69, 24);
+            clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // equpoToolStripMenuItem
+            // 
+            equpoToolStripMenuItem.Name = "equpoToolStripMenuItem";
+            equpoToolStripMenuItem.Size = new Size(70, 24);
+            equpoToolStripMenuItem.Text = "Equipo";
+            // 
+            // inventarioToolStripMenuItem
+            // 
+            inventarioToolStripMenuItem.Name = "inventarioToolStripMenuItem";
+            inventarioToolStripMenuItem.Size = new Size(139, 24);
+            inventarioToolStripMenuItem.Text = "Orden de Trabajo";
+            // 
+            // repuestoToolStripMenuItem
+            // 
+            repuestoToolStripMenuItem.Name = "repuestoToolStripMenuItem";
+            repuestoToolStripMenuItem.Size = new Size(85, 24);
+            repuestoToolStripMenuItem.Text = "Repuesto";
+            // 
+            // inventarioToolStripMenuItem1
+            // 
+            inventarioToolStripMenuItem1.Name = "inventarioToolStripMenuItem1";
+            inventarioToolStripMenuItem1.Size = new Size(89, 24);
+            inventarioToolStripMenuItem1.Text = "Inventario";
+            // 
+            // facturaToolStripMenuItem
+            // 
+            facturaToolStripMenuItem.Name = "facturaToolStripMenuItem";
+            facturaToolStripMenuItem.Size = new Size(70, 24);
+            facturaToolStripMenuItem.Text = "Factura";
             // 
             // FrmInicio
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(624, 316);
+            ClientSize = new Size(897, 616);
+            Controls.Add(menuStrip1);
             Font = new Font("Segoe UI", 10F);
             Name = "FrmInicio";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Inicio";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem administradorToolStripMenuItem;
+        private ToolStripMenuItem clienteToolStripMenuItem;
+        private ToolStripMenuItem equpoToolStripMenuItem;
+        private ToolStripMenuItem inventarioToolStripMenuItem;
+        private ToolStripMenuItem repuestoToolStripMenuItem;
+        private ToolStripMenuItem inventarioToolStripMenuItem1;
+        private ToolStripMenuItem facturaToolStripMenuItem;
     }
 }
